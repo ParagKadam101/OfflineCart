b2f6d01b5aafba688fdda6915587f9007bfa822d
Parag Kadam <parag.kadam101@gmail.com>
2019-02-25 00:25:18 +0530

    Improved formatting

--- src/components/cart.vue
+++ src/components/cart.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="container">
-        <ul class="list-group my-5" :key = "item.id" v-for="item in items">
+    <div class="container mt-5">
+        <ul class="list-group" :key = "item.id" v-for="item in items">
             <li  class="list-group-item">
                 {{item.title}} - {{item.price}}
                 <button v-on:click="$emit('remove-from-cart', item)" class="btn btn-sm btn-danger float-right">Remove from cart</button>
             </li>
         </ul>
-        <div class="card my-10 p-3">
+        <div class="card p-3 mt-5">
             <h1 class="text-center">$ {{cartValue}}</h1>
         </div>
 
